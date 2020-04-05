20709b7032221a5478752e38828c2136caae8600
IchiSamaFR <djordan-dsousa@hotmail.fr>
2020-04-05 20:38:37 +0200

    Ajoutez des fichiers projet.

---
+++ gsb_cloture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gsb_cloture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
